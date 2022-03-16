c22e896bd20dc20dc414fc51098140fe38d71427
Terry Carter <terry.carter@exprealty.net>
2022-03-16 15:20:51 -0500

    master: time update with one liners

--- time/time_func.py
+++ time/time_func.py
@@ -15,11 +15,15 @@ gm_time = time.gmtime()
 print("gm_time", gm_time)                                   # Print GMT current time '(tm_year=2022, tm_mon=3, tm_mday=16, tm_hour=18, tm_min=12, tm_sec=43, tm_wday=2, tm_yday=75, tm_isdst=0)'
 print(time.strftime('%Y-%m-%d', gm_time))                   # Print GMT current date only '2022-03-16'
 print(time.strftime('%Y-%m-%d', gm_time), "00:00:00+00:00") # Print GMT current date zero'd time  '2022-03-16 00:00:00+00:00'
+oneline_gm_time = (time.strftime('%Y-%m-%d', time.gmtime()) + " 00:00:00+00:00") # 2022-03-16 00:00:00+00:00
+print('oneline_gm_time', oneline_gm_time)
 print()
 
 print("Yesterday's date")
 yesterday_gm_time = time.gmtime(time.time() - 86400)
 print(time.strftime('%Y-%m-%d', yesterday_gm_time), "00:00:00+00:00") # Print yesterday's date, zero'd time '2022-03-15 00:00:00+00:00'
+oneline_yesterday_gm_time = (time.strftime('%Y-%m-%d', time.gmtime(time.time() - 86400))+ " 00:00:00+00:00") # 2022-03-15 00:00:00+00:00
+print("oneline_yesterday_gm_time", oneline_yesterday_gm_time)
 print()
 
 print('string to date conversion')
