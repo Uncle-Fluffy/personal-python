d70ee159a92780d29b6d874b7c579bbb226d9b9a
Terry Carter <terry.carter@exprealty.net>
2022-03-17 14:33:05 -0500

    tcd: cleaned up unneeded lines

--- send_daly_email/2nd_attempt/python_script/tcd_email.py
+++ send_daly_email/2nd_attempt/python_script/tcd_email.py
@@ -22,35 +22,18 @@ def GetDate():
         month = int(sys.argv[2])
         day = int(sys.argv[3])
         start_date = (str(datetime.datetime(year, month, day)) + "+00:00")
-        print(start_date)
 
         year2 = int(sys.argv[4])
         month2 = int(sys.argv[5])
         day2 = int(sys.argv[6])
         end_date = (str(datetime.datetime(year2, month2, day2)) + "+00:00")
-        print(end_date)
  
     elif len(sys.argv) == 4:    # if just the start date is given
         year = int(sys.argv[1])
         month = int(sys.argv[2])
         day = int(sys.argv[3])
-    #     start_date = utc.localize(datetime.datetime(year, month, day))
         start_date = (str(datetime.datetime(year, month, day)) + "+00:00")
-       # end_date = start_date + datetime.timedelta(days=1)
-       #end_date = (time.strftime('%Y-%m-%d', (str(datetime.datetime(year, month, day))- 86400)))
-        # foo = str(datetime.datetime(year, month, day))
-        # print("foo", foo)
-        # print(type(foo))
-        # foo2 = datetime.datetime(year, month, day) + datetime.timedelta(days=1)
-        # print("foo2", foo2)
-        # print(type(foo2))
-        # foo3 = str(datetime.datetime(year, month, day) + datetime.timedelta(days=1)) + "+00:00"
-        # print("foo3", foo2)
-        # print(type(foo3))
         end_date = str(datetime.datetime(year, month, day) + datetime.timedelta(days=1)) + "+00:00"
-
-        print(start_date)
-        print(end_date)
 
     else:   # No date given - assume yesterday
         end_date = (time.strftime('%Y-%m-%d', time.gmtime()) + " 00:00:00+00:00")
